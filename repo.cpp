4a426de3fd10fa22c0aef31b3fab63ce18b5eba4
margisj <37324267+margisj@users.noreply.github.com>
2018-11-14 00:52:07 -0800

    Created basic function headers

--- repo.cpp
+++ repo.cpp
@@ -17,13 +17,24 @@ using namespace std;
 
 Repo::Repo(string sPath, string tPath) {
 
+
+}
+
+void Repo::create(string sPath, string tPath) {
+	//creates a repo using a given source tree
 	this->sPath = sPath;
 	//Append root folder name to the target path
 	this->tPath = tPath + getSlash(this->sPath) + experimental::filesystem::path(sPath).stem().string();
 	//Create Manifest
 	this->manifest = new Manifest(tPath);
 	//Create initial leaf for root folder
-	this->head = new Leaf(this->sPath, this->tPath, this->manifest);
+	this->head = new Leaf(this->sPath, this->tPath, this->manifest);	
+}
+void Repo::checkIn(string sPath, string tPath) {
+	//updates an existing project tree
+}
+void Repo::checkOut(string sPath, string tPath, string manifest) {
+	//creates a repo using a given manifest
 }
 
 
--- repo.h
+++ repo.h
@@ -32,6 +32,10 @@ public:
 
 public:
 	Repo(string sPath, string tPath);
+	void create(string sPath, string tPath); //creates a repo using a given source tree
+	void checkIn(string sPath, string tPath); //updates an existing project tree
+	void checkOut(string sPath, string tPath, string manifest); //creates a repo using a given manifest
+
 	
 };
 
